7566dc6bdcbdac471886a0d49dd7834bac7dbf02
Alexandra Loynaz <alexandra.loynaz@correo.unimet.edu.ve>
2025-07-05 18:28:12 -0400

    Arreglos e implementacion de la ventana 1

--- Proyecto2/src/Principal/Main.java
+++ Proyecto2/src/Principal/Main.java
@@ -3,4 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
 */
 package Principal;
-
+import Interfaz.Ventana1;
+public class Main {
+    public static void main(String[] args) { 
+        Ventana1 ventana1 = new Ventana1(); 
+        ventana1.setVisible(true);
+        
+    
+    
+    
+    }
+}
